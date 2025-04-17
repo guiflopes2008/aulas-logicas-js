e052f74ff103e2c33b3126938527eb19fdd5c73e
GUILHERME FREITAS LOPES <guilherme_lopes151@estudante.sesisenai.org.br>
2025-04-16 22:19:23 -0300

    declaração de variaveis

---
+++ variaveis.js
@@ -0,0 +1,21 @@
+
+//declaração de variavel
+// var nome = "Guilherme"
+
+// var nome = "João"
+
+//declaração com let
+// let nome ="Guilherme"
+
+// nome="joao"
+
+// console.log(nome)
+
+
+//variavel declarada com const
+const nome = "Guilherme"
+
+nome = "João"
+
+console.log(nome)
+
