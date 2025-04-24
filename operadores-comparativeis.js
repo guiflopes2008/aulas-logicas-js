239440c2e5ff5ebe9dfdef60ca603a0935a8b22e
GUILHERME FREITAS LOPES <guilherme_lopes151@estudante.sesisenai.org.br>
2025-04-23 22:20:30 -0300

    feat: operadores em js

---
+++ operadores-comparativeis.js
@@ -0,0 +1,16 @@
+const valor01 = 10
+const valor02 = "20"
+const valor03 = 20
+const valor04 = 31
+// ==, === , > , < , >=,<=, != , !==
+
+
+// console.log(`O valor da variável é ${typeof valor01}`) //typeof é uma instrução para verificar o tipo da variavel
+
+// console.log(valor03 <= valor04)
+
+// console.log(valor04 % 2 == 0)
+
+const verificadorDeNumeroPar = valor04 % 2 === 0
+
+console.log(`${verificadorDeNumeroPar ? "valor par" : "valor Impar"}`)
---
+++ operadores-logicos.js
@@ -0,0 +1,16 @@
+// const valor01 = 50
+// const valor02 = 20
+
+// &&=>operador de conjunção  
+// || =>operador de disjunção
+// ! =>negação
+
+// console.log(`${valor01>20 || valor02>10}`)
+
+const TemCarteira = true
+const idade = 15
+const teste = TemCarteira && idade>18 
+
+console.log(`${teste ? "o candidato tem carteira" : "nao pode"}`)
+
+
